8a08b303296c100b2cee3fff1025383c3af6ff0b
gon <gone993@naver.com>
2021-03-01 20:41:00 +0900

    first commit

---
+++ src/main/java/com/gonlog/GonlogApplication.java
@@ -0,0 +1,13 @@
+package com.gonlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GonlogApplication.class, args);
+    }
+
+}
